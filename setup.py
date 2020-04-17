0494dc90b713c71c1e92b03d036a6af6e223d663
Mark Miller <725mrm@gmail.com>
2020-04-16 20:44:35 -0700

    Added required installs to the setup.py script

--- setup.py
+++ setup.py
@@ -17,4 +17,8 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    install_requires=[
+        'nbt',
+        'frozendict',
+    ]
 )
