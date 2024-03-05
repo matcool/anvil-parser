040508995da07edc2d2563a556e47e022ffd4062
Eric K <54124297+Nyveon@users.noreply.github.com>
2024-03-05 12:52:37 -0300

    refactor: move version constants to separate file

---
+++ anvil/versions.py
@@ -0,0 +1,15 @@
+# This version removes the chunk's "Level" NBT tag and moves all contained tags to the top level
+# https://minecraft.wiki/w/Java_Edition_21w43a
+VERSION_21w43a = 2844
+
+# This version removes block state value stretching from the storage
+# so a block value isn't in multiple elements of the array
+VERSION_20w17a = 2529
+
+# This version changes how biomes are stored to allow for biomes at different heights
+# https://minecraft.wiki/w/Java_Edition_19w36a
+VERSION_19w36a = 2203
+
+# This is the version where "The Flattening" (https://minecraft.wiki/w/Java_Edition_1.13/Flattening) happened
+# where blocks went from numeric ids to namespaced ids (namespace:block_id)
+VERSION_17w47a = 1451
